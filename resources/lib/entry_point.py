b5e615c5cd23b6c16a59b75f45ed5888b7400ab0
anxdpanic <anxdpanic@users.noreply.github.com>
2021-08-23 15:21:42 -0400

    add xbmc modules to ignored-modules and enable import-error check

--- resources/lib/entry_point.py
+++ resources/lib/entry_point.py
@@ -6,9 +6,9 @@
     See LICENSES/GPL-2.0-only.txt for more information.
 """
 
-from skinshorcuts import skinshortcuts
-from skinshorcuts.common import log
-from skinshorcuts.constants import ADDON_VERSION
+from skinshorcuts import skinshortcuts  # pylint: disable=import-error
+from skinshorcuts.common import log  # pylint: disable=import-error
+from skinshorcuts.constants import ADDON_VERSION  # pylint: disable=import-error
 
 log('script version %s started' % ADDON_VERSION)
 skinshortcuts.Main()
